8f4d2b7824782403f24deb35931e9386401d85e7
Danila Serpokrylov <serpokrylov.danila@mail.ru>
2023-05-08 11:09:41 +0000

    Dev

---
+++ src/styled.ts
@@ -0,0 +1,73 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    border: 0;
+  }
+
+  *, *:before, *:after {
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+  :focus, :active {
+    outline: none;
+  }
+  a:focus, a:active {
+    outline: none;
+  }
+  nav, footer, header, aside {
+    display: block;
+  }
+  html, body, #root {
+    height: 100%;
+    width: 100%;
+  }
+  
+  .modal_footer {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 4rem;
+  }
+  
+  #root {
+    position: relative;
+    z-index: 1;
+  }
+  #toast-root {
+    position: relative;
+    z-index: 2;
+  }
+  input::-ms-clear {
+    display: none;
+  }
+  input, button, textarea {
+    font-family: inherit;
+  }
+  button {
+    cursor: pointer;
+  }
+  button::-moz-focus-inner {
+    padding: 0;
+    border: 0;
+  }
+  a, a:visited {
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: none;
+  }
+  
+  .app-toast {
+    position: fixed;
+    z-index: 999999 !important;
+  }
+`;
+
+export const AppWrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+`;
